dfb30f5aa568bce387a41ab513c44c6c792fde14
Rafael Luiz de Araújo Oliveira <rafelluiz@gmail.com>
2021-01-27 07:28:51 -0300

    Ver todas as promoções
    Tarefa: https://trello.com/c/04T4AKql/5-ver-todas-as-promo%C3%A7%C3%B5es

--- app/controllers/promotions_controller.rb
+++ app/controllers/promotions_controller.rb
@@ -1,5 +1,20 @@
 class PromotionsController < ApplicationController
+  before_action :set_promotion, only: [:show]
+
   def index
     @promotions = Promotion.all
   end
+
+  def show; end
+
+  private
+
+  def set_promotion
+    @promotion = Promotion.find(params[:id])
+  end
+
+  def promotion_params
+    params.require(:promotion).permit(:name, :description,:code,:discount_rate,:coupon_quantity,:expiration_date)
+  end
+
 end
