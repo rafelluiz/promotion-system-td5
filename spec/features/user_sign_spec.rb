bc987484f347b269d6769236d93ccacbe5697307
Rafael Luiz de Araújo Oliveira <rafelluiz@gmail.com>
2021-02-10 10:57:58 -0300

    Teste para inscrição de usuário

--- spec/features/user_sign_spec.rb
+++ spec/features/user_sign_spec.rb
@@ -37,4 +37,21 @@ feature 'User sign in' do
       expect(page).to have_link 'Entrar'
     end
   end
+
+  scenario 'and sign up' do
+    visit root_path
+    click_on 'Entrar'
+    click_on 'Sign up'
+    within('form') do
+      fill_in 'E-mail',with: 'email@example.com'
+      fill_in 'Senha',with: 'password'
+      fill_in 'Confirme sua senha',with: 'password'
+      click_on 'Inscrever-se'
+    end
+
+    expect(page).to have_content 'email@example.com'
+    expect(page).to have_content 'Bem vindo! Você realizou seu registro com sucesso.'
+    expect(page).to have_link 'Sair'
+    expect(page).not_to have_link 'Entrar'
+  end
 end
