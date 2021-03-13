703ba4768f13beb8339d6d4fec5d60cb9c428674
Rafael Luiz <rafelluiz@gmail.com>
2021-03-13 09:03:37 -0300

    JavaScript no Rails

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -1,6 +1,15 @@
 class CouponsController < ApplicationController
   before_action :set_coupon, only: %i[inactivate]
 
+  def index
+    @coupons = Coupon.all
+  end
+
+  def search
+    @coupons = Coupon.where('code LIKE ?', "%#{params[:q]}%")
+    render json: @coupons
+  end
+
   def inactivate
     @coupon.inactive!
     #raise Error if @coupon Entender depois pq isso quebrou
